031c2a6e65d535b54ef723db77ee06d36f327a1f
faisalansari0367 <faisalansari0367@users.noreply.github.com>
2021-02-18 18:05:17 +0530

    added changeLog version 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,6 @@
 - First version of plugin
 
 ## 0.0.2
-- Added more desciptive example 
+- Added more descriptive example 
+## 0.0.3
+- Added FileObserver to watch for Root Storage like '/storage/emulated/0' and refactor code to make it more cleaner.
--- lib/storage_details.dart
+++ lib/storage_details.dart
@@ -7,9 +7,11 @@ import 'package:flutter/services.dart';
 class StorageDetails {
   static const MethodChannel _channel = const MethodChannel('storage_details');
   static const String _getStorageDetails = 'getStorageDetails';
+  static const EventChannel _eventChannel = const EventChannel('event_channel');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
+
     return version;
   }
   /// this method provides you the path of internal and removable sd card and it also provides the used, free, available space
@@ -24,8 +26,26 @@ class StorageDetails {
     return storage;
   }
 
+  static Future<bool> deleteWhenError(List<String> paths) async {
+    try {
+      var result = await _channel.invokeMethod(
+        'deleteWhenError',
+        <String, List<String>>{'paths': paths},
+      );
+      return result;
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
 
+  static Stream<dynamic> get watchFilesForChanges async* {
+    await for (var event in _eventChannel.receiveBroadcastStream()) {
+      yield event;
+    }
+  }
 }
+
+
 
 
 class Storage {
